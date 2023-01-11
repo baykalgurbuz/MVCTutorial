4aaa28748f69ee03d06b6088a344e5a404ecdb87
EnesG <EnesG@DESKTOP-CVLKPKE>
2023-01-11 16:47:00 +0300

    Add project files.

---
+++ MVCP/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCP.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Layout kısmı sabit kalan kısıma denir.Eski ismi Masterpage.
+        //Değişen kısımlar ise Content'dir.
+        public ActionResult Index()//Metod varsa View tarfında karşılığı olmalıdır.
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
